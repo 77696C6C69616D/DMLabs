bfa8f24af3ea3a33b5f936fb56dff7f7b45b81cf
Dave <melza2000life@gmail.com>
2022-12-18 14:35:30 +0200

    suffering

--- DMLab5.py
+++ DMLab5.py
@@ -1,34 +1,67 @@
 alpmatrix = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'k']
-nummatrix = [1, 2, [3, 4], [5, 6], 7, 8]
+NodeNumber = 8
+branch = [3, 5]
 
-
-def matrix_of_len (inmatrix): 
-    outmatrix = init_out(inmatrix)
+def matrix_of_len (inmatrix, tmp = []): 
+    cout = [
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+        [0,0,0,0,0,0,0,0],
+    ]
+    substractor = len(branch)
     
-    for inner in range (0, len(outmatrix)):
-        
-        for inner_inner in range (1, len(outmatrix) + 1):
-            
-            
+    for inner in range (0, NodeNumber):
+        for inner_inner in range (0, NodeNumber):
+            if inner != inner_inner:   
+                
+                if inner_inner + 1 in branch: 
+                    cout[inner][inner_inner] = None
+                    continue
+                    
+                
+                
+                
+                if inner_inner + 1 > NodeNumber/2: 
+                    cout[inner][inner_inner] = inner_inner + 1 - inner + 1 - substractor
+                    continue
+                
+                
+                
+                cout[inner][inner_inner] = inner_inner - 1 - inner - 1
+                    
+                    
+
+                    
+                    
+                    
             None
+        break
+            
+            
+            
+            
+    # [0, 1, 2, 2, 3, 3, 4, 5],
+    # [1, 0, 1, 1, 2, 2, 3, 4],
+    # [2, 1, 0, 1, 1, 1, 2, 3],
+    # [2, 1, 1, 0, 2, 1, 2, 3],
+    # [3, 2, 1, 2, 0, 1, 2, 3],
+    # [3, 2, 1, 1, 1, 0, 1, 2],
+    # [4, 3, 2, 2, 2, 1, 0, 1],
+    # [5, 4, 3, 3, 3, 2, 1, 0]]
     
-    for i in outmatrix:
+    
+    for i in cout: 
         print(i)
     
-    return outmatrix
+        
+    exit()    
 
-def init_out(input, cout = [], tmp = []): 
-    length = len(input)
-    for i in input:
-        if str(type(i)) == "<class 'list'>":
-            length += len(i) - 1
-    for inner in range (0, length):    
-        tmp.append(0)
-    for inner_inner in range (0, length): 
-        cout.append(tmp)
-    return cout
-
-def GetMaxNode(inp, cout = []): 
+def GetMaxNodesLen(inp, cout = []): 
     [cout.append(max(i)) for i in inp]
     return cout
 
@@ -46,19 +79,7 @@ def GetRadius (MaxNodeLen):
 def GetDiameter (MaxNodeLen):     
     return max(MaxNodeLen)
 
-cin = [
-[0, 1, 2, 2, 3, 3, 4, 5],
-[1, 0, 1, 1, 2, 2, 3, 4],
-[2, 1, 0, 1, 1, 1, 2, 3],
-[2, 1, 1, 0, 2, 1, 2, 3],
-[3, 2, 1, 2, 0, 1, 2, 3],
-[3, 2, 1, 1, 1, 0, 1, 2],
-[4, 3, 2, 2, 2, 1, 0, 1],
-[5, 4, 3, 3, 3, 2, 1, 0]]
-
-tmp = (GetMaxNode(cin))
-
-print('\n> Граф: '
+print('\n> Граф: \n'
 """
        c      e
        o------o
@@ -69,10 +90,17 @@ a   b\ |      | f
        d         /
               k o
 """)
+print("""
+o---o---o---o---o---o
+a   b   d   f   g   k
+        c---e
+""")
+
 print('> Матриця графа:\n')
-matrix_of_len(nummatrix)
+matrix_of_len(NodeNumber)
 print()
 
+tmp = GetMaxNodesLen(cin)
 print('> Центр графа:', GetCenter(tmp))
 print('> Периферійні вершини:', GetPerif(tmp))
 print('> Радіус графа R(G):', GetRadius(tmp))
